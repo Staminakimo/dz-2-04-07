3040c55d10c1bc0eeebba7af66668e6c557624ee
Юлька <stamina_kimo@mail.ru>
2022-07-04 10:51:17 +0300

    решение 1 задачи

---
+++ z-1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine($"Напиши любое трехзначное число: ");
+string number = Console.ReadLine();
+int usNumber = int.Parse(number);
+
+int secondDigit = usNumber / 10;  
+int findDigit = secondDigit % 10;  
+
+Console.WriteLine($"{findDigit}"); 
